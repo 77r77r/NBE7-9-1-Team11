85e9246ca6e8fdc4c2309db65b1024e219f37b1f
heygeeji <heygeeji@gmail.com>
2025-09-24 18:38:55 +0900

    [BE9]이혜지: 주문 내역 조회 API 구현

---
+++ src/main/java/com/cafe/domain/order/details/controller/OrderDetailsController.java
@@ -0,0 +1,44 @@
+package com.cafe.domain.order.details.controller;
+
+import com.cafe.domain.member.entity.Member;
+import com.cafe.domain.order.details.dto.OrderDto;
+import com.cafe.domain.order.details.service.OrderDetailsService;
+import com.cafe.global.rq.Rq;
+import com.cafe.global.rsData.RsData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/order")
+public class OrderDetailsController {
+
+    private final OrderDetailsService orderDetailsService;
+    private final Rq rq;
+
+    @GetMapping("/details")
+    public RsData<?> getOrderDetails() {
+        Member actor = rq.getMember();
+
+        List<OrderDto> orders = orderDetailsService.getOrdersByApiKey(actor.getApiKey());
+
+        if (orders.isEmpty()) {
+            return new RsData<>(
+                    "204-1",
+                    "주문 내역이 존재하지 않습니다."
+            );
+        }
+
+        return new RsData<>(
+                "200-1",
+                "주문 내역 조회 성공",
+                orders
+        );
+    }
+
+
+}
