5943f614154459b5993c1bd189ae8713bc3d4706
heygeeji <heygeeji@gmail.com>
2025-09-24 18:52:36 +0900

    [BE9]이혜지: findByMember 회원별 주문 내역 조회

---
+++ src/main/java/com/cafe/domain/order/order/repository/OrderRepository.java
@@ -0,0 +1,15 @@
+package com.cafe.domain.order.order.repository;
+
+import com.cafe.domain.member.entity.Member;
+import com.cafe.domain.order.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    List<Order> findByMember(Member member);
+
+}
