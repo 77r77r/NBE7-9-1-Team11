353b1d5ebac63f619e5554f213327df4c4462296
77r77r <codingworld@naver.com>
2025-09-24 15:43:10 +0900

    [ãBE9]노미경: Member, Product Merge

--- src/main/java/com/cafe/global/initData/BaseInitData.java
+++ src/main/java/com/cafe/global/initData/BaseInitData.java
@@ -2,6 +2,7 @@ package com.cafe.global.initData;
 
 import com.cafe.domain.member.entity.Member;
 import com.cafe.domain.member.service.MemberService;
+import com.cafe.domain.product.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -17,13 +18,18 @@ public class BaseInitData {
     @Autowired
     @Lazy
     private BaseInitData self;
+
     private final MemberService memberService;
     private static final String SYSTEM_EMAIL = "elon@musk.com";
+
+    @Autowired
+    private ProductService productService;
 
     @Bean
     ApplicationRunner initDataRunner() {
         return args -> {
             self.work1();
+            self.productInitData(); // 상품 초기 데이터 등록
         };
     }
 
@@ -38,6 +44,19 @@ public class BaseInitData {
                 "경기도 화성시", "12345"
         );
         system.grantAdmin();
+    }
+
+    /**
+     * 상품 초기데이터 등록
+     */
+    @Transactional
+    public void productInitData() {
+        if (productService.count() > 0) return;
+
+        productService.register("Colombia Nariño", 5100, "콜롬비아");
+        productService.register("Colombia Quindío", 5600, "콜롬비아", 50);
+        productService.register("Brazil Serra Do Caparaó", 6300, "브라질", 20);
+        productService.register("Ethiopia Yirgacheffe", 6800, "에티오피아", 1);
     }
 
 }
