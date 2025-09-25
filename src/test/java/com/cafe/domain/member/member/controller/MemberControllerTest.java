4e7adad136bf2c6d45796ba2cf5e20694fe88db1
Labtory-82 <chnny9@gmail.com>
2025-09-25 10:49:19 +0900

    [BE9]이유찬: import 변경

--- src/test/java/com/cafe/domain/member/member/controller/MemberControllerTest.java
+++ src/test/java/com/cafe/domain/member/member/controller/MemberControllerTest.java
@@ -1,8 +1,5 @@
 package com.cafe.domain.member.member.controller;
 
-import com.cafe.domain.member.member.repository.MemberRepository;
-import jakarta.servlet.http.Cookie;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +11,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -31,9 +24,6 @@ public class MemberControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @Autowired
-    private MemberRepository memberRepository;
-
     private String apiVersion = "v1";
 
     @Test
@@ -43,7 +33,7 @@ public class MemberControllerTest {
         String email = "yuchan123@gmail.com";
         String password = "asdf1234!";
         String nickname = "이 유 찬";
-        String address = "경기도 성남시 태평동 123-4";
+        String address = "경기도 성남시 수정구 태평5동 123-4";
         String postalCode = "18572";
 
         ResultActions resultActions = mvc
@@ -66,9 +56,10 @@ public class MemberControllerTest {
         resultActions
                 .andExpect(handler().handlerType(MemberController.class))
                 .andExpect(handler().methodName("join"))
+                .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.resultCode").value("201-1"))
                 .andExpect(jsonPath("$.msg").value("회원가입이 완료되었습니다. %s님 환영합니다!".formatted(nickname)))
-                .andExpect(jsonPath("$.data.memberDto.id").value(2))
+                .andExpect(jsonPath("$.data.memberDto.id").value(3))
                 .andExpect(jsonPath("$.data.memberDto.email").value(email))
                 .andExpect(jsonPath("$.data.memberDto.name").value(nickname))
                 .andExpect(jsonPath("$.data.memberDto.address").value(address))
@@ -82,10 +73,10 @@ public class MemberControllerTest {
     @DisplayName("회원 가입, 이미 존재하는 이메일로 가입")
     void t2() throws Exception {
 
-        String email = "elon@musk.com";
+        String email = "gen@init.com";
         String password = "asdf1234!";
         String nickname = "이 유 찬";
-        String address = "경기도 성남시 태평동 123-4";
+        String address = "경기도 성남시 수정구 태평5동 123-4";
         String postalCode = "18572";
 
         ResultActions resultActions = mvc
